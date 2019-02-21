5710223a07635a602fc69008c638d7c6bf22ec81
Matthew Yee <matthewyee@Jacky-Man.home>
2019-02-21 10:17:37 -0500

    fetch api call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = ({
+      country: '',
+      capital: '',
+      region: '',
+      subregion: ''
+    })
+  }
+
+  async componentDidMount() {
+    const request = await axios.get('https://restcountries.eu/rest/v2/all')
+    const response = request.data 
+    console.log(response)
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Country/Capital Data Multi-Search Service</h1>
       </div>
     );
   }
