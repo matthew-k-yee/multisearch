e9020191ef761010ea8382a1cc56bf55efc11606
Matthew Yee <matthewyee@Jacky-Man.local>
2019-02-22 01:12:26 -0500

    refactor code, multi filter works

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Search from './components/Search'
+import Table from './components/Table'
 import './App.css';
 import axios from 'axios';
 
@@ -7,7 +7,11 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = ({
-      data: []
+      data: [],
+      countrySearch: '',
+      capitalSearch: '',
+      regionSearch: '',
+      subregionSearch: ''
     })
   }
 
@@ -32,11 +36,64 @@ class App extends Component {
     })
   }
 
+  handleCountryChange = e => {
+    this.setState({
+      countrySearch: e.target.value
+    })
+  }
+
+  handleCapitalChange = e => {
+    this.setState({
+      capitalSearch: e.target.value
+    })
+  }
+
+  handleRegionSearch = e => {
+    this.setState({
+      regionSearch: e.target.value
+    })
+  }
+
+  handleSubregionSearch = e => {
+    this.setState({
+      subregionSearch: e.target.value
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1>Country/Capital Data Multi-Search Service</h1>
-        <Search data={this.state.data } />
+        <input 
+            type="text" 
+            name='country'
+            placeholder='Filter by country' 
+            onChange={this.handleCountryChange}  
+            />
+          <input 
+            type="text" 
+            name='capital'
+            placeholder='Filter by capital' 
+            onChange={this.handleCapitalChange}  
+            />
+          <input 
+            type="text" 
+            name='region'
+            placeholder='Filter by region' 
+            onChange={this.handleRegionSearch}  
+            />
+          <input 
+            type="text" 
+            name='subregion'
+            placeholder='Filter by subregion' 
+            onChange={this.handleSubregionSearch}  
+            />
+          <Table 
+            data={this.state.data}
+            countrySearch={this.state.countrySearch}
+            capitalSearch={this.state.capitalSearch}
+            regionSearch={this.state.regionSearch}
+            subregionSearch={this.state.subregionSearch}
+            />
       </div>
     );
   }
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,37 @@
 import React from 'react' 
 
 const Table = props => {
-  const data = props.data
+
+    let data = props.data
+    const countrySearch = props.countrySearch.toLowerCase()
+    const capitalSearch = props.capitalSearch.toLowerCase()
+    const regionSearch = props.regionSearch.toLowerCase()
+    const subregionSearch = props.subregionSearch.toLowerCase()
+
+    if (countrySearch !== '') {
+      data = data.filter(item => (
+        item.country.toLowerCase().match(countrySearch)
+      ))
+    }
+
+    if (capitalSearch !== '') {
+      data = data.filter(item => (
+        item.capital.toLowerCase().match(capitalSearch)
+      ))
+    }
+
+    if (regionSearch !== '') {
+      data = data.filter(item => (
+        item.region.toLowerCase().match(regionSearch)
+      ))
+    }
+
+    if (subregionSearch !== '') {
+      data = data.filter(item => (
+        item.subregion.toLowerCase().match(subregionSearch)
+      ))
+    }
+
   const list = data.map((country, index) => {
     return (
       <tr key={index}>
